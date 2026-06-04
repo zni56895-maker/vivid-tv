e109ca6559c400d633e5ab8120eea645bdf7d4d3
Vivid TV Dev <dev@vividtv.local>
2026-06-04 23:47:37 +0800

    chore: bump version to 0.2.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.vividtv"
         minSdk = 21
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "0.2.0.0"
     }
 
     buildFeatures {
